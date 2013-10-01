70de6dc653ca375d00ddae8077920ad106ca0c0d
sirmarcel <me@lumenlog.com>
2013-10-01 23:30:40 +0200

    Make paths right

--- lib/podding-cli.rb
+++ lib/podding-cli.rb
@@ -7,4 +7,5 @@ require_relative 'settings/load_settings'
 require_relative 'settings/write_settings'
 
 # Load subcommands
-require_relative 'episode'
+require_relative 'commands/episode'
+require_relative 'commands/settings'
