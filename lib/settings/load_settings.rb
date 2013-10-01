33c0dcd1635743ed3f0cc6fb7004fafa59da0d66
sirmarcel <me@lumenlog.com>
2013-10-01 23:30:19 +0200

    default_project -> default

--- lib/settings/load_settings.rb
+++ lib/settings/load_settings.rb
@@ -4,6 +4,6 @@ require 'settingslogic'
 
 class Settings < Settingslogic
   source File.join(Dir.home, ".podding")
-  self.default_project ? namespace( self.default_project ) : namespace( "default" ) # set namespace to the one in ~/.podding
+  self.default ? namespace( self.default ) : namespace( "default" ) # set namespace to the one in ~/.podding
   reload! # reload with newly set namespace
 end
