2366250acad802438a9e0a5f6b1654e90f0a606f
sirmarcel <me@lumenlog.com>
2013-10-10 17:56:30 +0200

    Made preflight settings setup shorter, and a bit more friendly.

--- lib/lib/preflight/settings.rb
+++ lib/lib/preflight/settings.rb
@@ -5,26 +5,22 @@ unless File.exists?( settings_file )
     desc "initial setup","initial setup"
     def initial_setup
       say "It looks like you haven't set anything up yet. (~/.podding is missing.)", :red
-      exit unless "y" == ask( "Begin initial setup? (y/n)")
-      File.open( settings_file, 'w' ) # touch
-      need_to_install = ask( "Do you already have a podding installation (we call them 'projects') on this machine? (y/n)" )
-      if need_to_install == "y"
-        name = ask("Cool. What would you like to call it from now on? Use a short, easy-to-type name.")
-        path = ask("Where is this project?")
-        path = File.expand_path( path )
-        create_namespace(name, path)
-        set_default_namespace(name)
-        say "Great. Set #{ name } as default project.", :green
-        say "In the future, use podding project new NAME PATH to create new projects, and podding use NAME to set them as default."
-        say "Have fun! <3"
-        exit
-      elsif need_to_install == "n"
-        # TODO: Setup
-      else
-        say "Sorry, what do you mean by #{ need_to_install }? Please try again."
-      end
+      exit unless "y" == ask( "Create a settings file? (y/n)")
+      say "Podding installations are called projects and get listed in ~/.podding."
+      say "To create a settings file, we'll need to create a project."
+      name = ask( "What would you like to call the project? Use a short, easy-to-type name." )
+      path = ask("Where is this project? (I need a path.)")
+      path = File.expand_path( path )
+      create_namespace(name, path)
+      puts settings_to_hash
+      set_default_namespace(name)
+      say "Great. Set #{ name } as default project.", :green
+      say "You can easily add new projects using podding add NAME, and switch between them with podding use NAME."
+      say "You can also edit ~/.podding, it's rather straightforward."
+      say "If you haven't actually installed podding, you should do that now in #{ path }."
     end
   end
 
   Setup.new.initial_setup
+  exit
 end
