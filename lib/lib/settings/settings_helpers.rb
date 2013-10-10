0ac3b83b6610cf7653026812f6aaed849e312732
sirmarcel <me@lumenlog.com>
2013-10-10 17:54:41 +0200

    Fixed some stuff

--- lib/lib/settings/settings_helpers.rb
+++ lib/lib/settings/settings_helpers.rb
@@ -9,16 +9,27 @@ def settings_file
 end
 
 def settings_to_hash
-  {} || YAML.load(File.read(settings_file))
+  if File.exists?( settings_file )
+    return YAML.load(File.read(settings_file))
+  else
+    return {}
+  end
 end
 
 def hash_to_settings( settings )
-  File.open(settings_file, 'w') { |file| file.write settings.to_yaml }
+  File.open(settings_file, 'w') { |file| file.write( settings.to_yaml )}
+end
+
+def add_key_to_settings( key, value )
+  settings = settings_to_hash
+  settings[ key ] = value
+  hash_to_settings(settings)
 end
 
 def set_default_namespace(name)
-  settings = settings_to_hash
+  settings = {}
   settings["default"] = name
+  settings = settings.merge(settings_to_hash) # makes sure that default ends up on top of the yaml
   hash_to_settings( settings )
   Settings.reload! if defined?( Settings )
 end
@@ -28,7 +39,5 @@ def namespace_exists?(name)
 end
 
 def create_namespace(name, path)
-  settings = settings_to_hash
-  settings[name] = { "podding_root" => path }
-  hash_to_settings(settings)
+  add_key_to_settings( name, { "podding_root" => path } )
 end
